949fcf9983335d97fd1c0722cfc1c431b5531b08
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-08-16 23:56:25 -0500

    User scehma

---
+++ contactapi/web/models/user_model.ex
@@ -0,0 +1,16 @@
+defmodule Contactapi.User do
+  """
+  User Model
+  """
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+    timestamps
+  end
+
+
+end
