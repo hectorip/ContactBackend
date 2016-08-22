cf1343dca115cd0b6f67b3f824f1f0b9111b11ed
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-08-22 08:51:53 -0500

    fixing prod config

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :contactapi, Contactapi.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "morning-thicket-49816.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :hello_phoenix, Contactapi.Repo,
