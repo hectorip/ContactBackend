4253448200613d47aae1a96d7c4bee811341b413
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-08-21 23:56:36 -0500

    Create function for user

--- contactapi/web/controllers/user_controller.ex
+++ contactapi/web/controllers/user_controller.ex
@@ -16,4 +16,18 @@ defmodule Contactapi.UserController do
     changeset = User.changeset(%User{})
     render conn, "new.html", changeset: changeset
   end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.registration_changeset(%User{}, user_params)
+    case Repo.insert(cangeset) do
+      {:ok, user} ->
+        conn
+        #|> Contactapi.Auth.login(user)
+        |>put_plash(:info, "#{user.name} has been created")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        conn
+          |> put_flash(:error, "Check the errors below")
+          |> render("new.html", changeset: changeset)
+  end
 end
